5cb019d2de44d747b6b8762ae94962f6f3dd73c8
Peter V <98245483+p5quared@users.noreply.github.com>
2022-12-14 20:30:29 -0500

    testing latest server updates

--- src/routes/games/testing/+page.svelte
+++ src/routes/games/testing/+page.svelte
@@ -1,14 +1,26 @@
 <script>
-    let product = {};
+    let count = 0;
 
     async function getData() {
         const response = await fetch('/api/testing');
-        product = await response.json();
+        let product = await response.json();
 
         console.log(product)
+        count = product.count
     }
 
+    async function raiseCount() {
+        const response = await fetch('/api/testing', {
+            method: 'POST'
+        })
+
+        let data = await response.json();
+
+        count = data.count
+    }
 </script>
 
 <h1>Testing Page!</h1>
-<button on:click={() => getData()}> GET DATA! </button>
+<h1>Count: {count}</h1>
+<button on:click={() => getData()}>Check Count!</button>
+<button on:click={() => raiseCount()}>Raise Count!</button>
