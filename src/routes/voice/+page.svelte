7b03bc087ea5a0cc2ae3440993cd8df52fe8b05b
Peter V <98245483+p5quared@users.noreply.github.com>
2022-12-27 00:22:37 -0500

    at least it compiles now

--- src/routes/voice/+page.svelte
+++ src/routes/voice/+page.svelte
@@ -1,1 +1,18 @@
-<h1>Voice Page</h1>
+<script>
+
+    let valid = true;
+    try {
+        let recognition = window.SpeechRecognition || webkitSpeechRecognition;
+    } catch (e) {
+        console.error(e);
+        valid = false;
+    }
+    const SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+    const SpeechGrammarList = window.SpeechGrammarList || webkitSpeechGrammarList;
+    const SpeechRecognitionEvent = window.SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
+
+    const destinations = ['appointment', 'games', 'tictactoe', 'matching', 'home', 'schedule', 'contact'];
+    const grammar = `#JSGF V1.0; grammar destinations; public <destination> = ${destinations.join(' | ')};`
+</script>
+
+<h1>{valid}</h1>
