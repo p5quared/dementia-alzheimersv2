2a857c513f6f9133801235d5bd54f63eefe187bd
Peter V <98245483+p5quared@users.noreply.github.com>
2023-01-31 19:01:11 -0500

    phone -> reminders. deleted old page; currently used for testing the VoiceCommands component

--- src/routes/reminders/+page.svelte
+++ src/routes/reminders/+page.svelte
@@ -1,77 +1,18 @@
 <script>
-    let videoSource = null;
-    let loading = false;
-    let loaded = false;
-    const videoCamara = async () => {
-        try {
-            loading = true;
-            videoSource.srcObject = await navigator.mediaDevices.getUserMedia({
-                video: true,
-            });
-            videoSource.play();
-            loading = false;
-            loaded = true;
-        } catch (error) {
-            console.log(error);
-        }
-    };
+    import VoiceCommands, {startRecognition} from '../../components/VoiceCommands.svelte';
+   startRecognition();
 </script>
 
 
-
-<div class="wrapper">
-    <img id="chess" src="/images/chessboard.png" alt="placeholder for games; chess">
-    <div class="video-container">
-        <!-- svelte-ignore a11y-media-has-caption -->
-        <video bind:this={videoSource}></video>
-
-        <div class="buttons">
-            <button on:click={videoCamara}>Start Video</button>
-            <a class="return-button" href="/">Return</a>
-        </div>
-    </div>
+<div id="page">
+    <VoiceCommands />
 </div>
 
+
 <style>
-    .wrapper {
-        display: flex;
-        align-items: center;
-        gap: 10px;
-        background-color: var(--color-offwhite);
-    }
-    .video-container {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
+    #page {
         height: 100vh;
+        width: 100vw;
+        color: darkgoldenrod;
     }
-    #chess {
-        height: 100vh;
-    }
-    video {
-        outline: solid;
-        width: 625px;
-    }
-    .buttons{
-        display: flex;
-        justify-content: space-around;
-        align-items: center;
-        margin-inline: 8em;
-        padding-top: 3em;
-    }
-    button {
-        background-color: #3C6E71;
-        color: var(--color-offwhite);
-        padding: 0.7em;
-        font-size: 18px;
-    }
-    .return-button {
-        background-color: #3C6E71;
-        color: var(--color-offwhite);
-        font-size: 18px;
-        padding: 0.75em;
-        border: solid black;
-    }
-
-
 </style>
