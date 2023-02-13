1428447e15b2fb9c7d679a1a6dbab92dc614aef7
Peter V <98245483+p5quared@users.noreply.github.com>
2023-02-12 21:38:10 -0500

    copy of other tic-tac-toe endpoint

---
+++ src/routes/api/tictactoe_private/+server.ts
@@ -0,0 +1,21 @@
+import type { RequestEvent } from '@sveltejs/kit';
+
+let global_game = {
+	history: [
+		{
+			board: Array(9).fill('')
+		}
+	],
+	xIsNext: true,
+	stepNumber: 0
+};
+
+export const GET = () => {
+	console.log(JSON.stringify(global_game));
+	return new Response(JSON.stringify(global_game));
+};
+
+export async function POST({ request }: RequestEvent) {
+	global_game = await request.json();
+	return new Response(JSON.stringify(global_game));
+}
